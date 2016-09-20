13e0a8a37a87749e0afe28a8f588e183ee08888a
Angel Del Castillo <jubu201504@gmail.com>
2016-09-20 15:21:58 -0500

    custom pprof port

---
+++ profiling/custom/custom.go
@@ -0,0 +1,48 @@
+// Package custom contains a server for net profiling listening
+// on port.
+/*
+	Use it import this way:
+	custom.Listen(port int)
+*/
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Angel Del Castillo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+package custom
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+
+	// import pprof
+	_ "net/http/pprof"
+)
+
+// Listen will start a server on port.
+func Listen(port int) {
+	runtime.SetBlockProfileRate(1)
+	go func(p int) {
+		log.Printf("Multi : web pprofiling enabled : listening :%v", p)
+		log.Println(http.ListenAndServe(fmt.Sprintf(":%v", p), nil))
+	}(port)
+}
